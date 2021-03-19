56287bddf33990132aa3580a1a7bc4583fc80c45
Larissa Flores Foti <larissaflores19@gmail.com>
2021-03-18 22:23:02 -0300

    Template images added

--- pinksetup/views.py
+++ pinksetup/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from pinksetup.models import Pinksetup
 
 
 def home(request):
-    return TemplateResponse(request, 'index.html')
+    return TemplateResponse(request, 'index.html', {'items': Pinksetup.objects.all()})

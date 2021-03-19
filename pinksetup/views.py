646dfa66708a9f1b0996d2362fe53270f4936032
Larissa Flores Foti <larissaflores19@gmail.com>
2021-03-18 21:12:23 -0300

    Bootstrap added

--- pinksetup/views.py
+++ pinksetup/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
 
 def home(request):
-    return HttpResponse("pinksetup for girls")
+    return TemplateResponse(request, 'index.html')
